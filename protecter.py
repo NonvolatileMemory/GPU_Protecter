83e56cecf9137a3c2954b8f352c7086bf7110c39
Cunxiao Du <cnsdunm@gmail.com>
2017-12-12 20:57:59 +0800

    Add files via upload

---
+++ protecter.py
@@ -0,0 +1,22 @@
+import os
+import time
+while(True):
+	process_list = []
+	c = os.popen("nvidia-smi")
+	t = c.read()
+	t = t.relace("\n","")
+	t = t.replace("|","")
+	t = t.replace("-","")
+	t = t.replace("=","")
+	t = t.replace("+","")
+	t = t.replace("/","")
+	t = t.split()
+	i = t.index('Usage')
+	while(i<len(t)-1):
+		process_list.append(t[i])
+		i = 5 + i
+	for process in process_list:
+		if(process_list.count(process)>1):
+			os.system("kill -9 "+process)
+			#print("kill -9 "+process)
+	time.sleep(300)
